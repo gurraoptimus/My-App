3ca6320054bf0a71eed70b8f97de0315f9ccbaef
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-11-05 00:51:06 +0100

    src

---
+++ src/java.js
@@ -0,0 +1,22 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "user" && password == "' or '1'='1'"){
+alert ("Login success");
+window.location = "https://www.arnflo.se"; // Redirecting to other page.
+return true;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").enabled = true ;
+document.getElementById("password").enabled = true;
+document.getElementById("submit").enabled = true;
+return true;
+}
+}
+}
